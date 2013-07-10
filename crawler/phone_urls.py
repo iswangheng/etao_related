18d8dfaa5e9ef16bcc8680524f4c037614af92d0
iswangheng <iswangheng@gmail.com>
2013-07-10 18:19:57 +0800

    update!! since s.etao.com is blocked...

--- crawler/phone_urls.py
+++ crawler/phone_urls.py
@@ -1,8 +1,9 @@
 # coding:utf-8
+import time
+import codecs
 import logging
 import os
 import logging.config
-import threading
 from ConfigParser import ConfigParser
 from bs4 import BeautifulSoup
 # http_helper is deprecated from July 9th, 2013
@@ -29,36 +30,71 @@ def get_phone_urls(init_url):
     """
     all_phone_urls = []
     next_page_url = init_url
-    total_phone_pages = 30
-    ONE_PAGE_NUM = 36
+    total_phone_pages = 40
     to_visit_urls = []
     while total_phone_pages:
-        print 'total_phone_pages: ', total_phone_pages
         to_visit_urls.append(next_page_url)
-        ori_phone_index = int(next_page_url[next_page_url.find("&s=")+len("&s="): next_page_url.find("&size=")])
-        new_phone_index = ori_phone_index + ONE_PAGE_NUM
-        ori_substr = "&s=%d" % ori_phone_index
-        new_substr = "&s=%d" % new_phone_index
+        ori_page = int(next_page_url[next_page_url.find("&p=")+len("&p="):])
+        new_page = ori_page + 1
+        ori_substr = "&p=%d" % ori_page
+        new_substr = "&p=%d" % new_page
         next_page_url = next_page_url.replace(ori_substr, new_substr)
         total_phone_pages -= 1
+    workers = [i+1 for i in range(len(to_visit_urls))]
+    worker_index = 0
     for to_visit in to_visit_urls:
-        varys.add_async(url=to_visit, callback=process_single_page, data=None, args=all_phone_urls)
-    while len(all_phone_urls) < 1008:
-        pass
-    print 'length of all_phone_urls is: ', len(all_phone_urls)
+        worker_index += 1
+        varys.add_async(url=to_visit, callback=process_single_page, data=None,
+                        args=[all_phone_urls, worker_index, workers, to_visit])
+    while len(workers) > 1:
+        time.sleep(3)
+        print 'current workers: ', workers
+    print 'Done!!! Length of all_phone_urls is: ', len(all_phone_urls)
     return all_phone_urls
 
 
 def process_single_page(content, args):
     """ get all the phone urls in the phone search page (36 phone urls in one single search page)
     """
+    all_phone_urls = args[0]
+    worker_index = args[1]
+    workers = args[2]
+    visit_url = args[3]
+    # save_page(str(worker_index) + 'worker.html', content)
     single_page_soup = BeautifulSoup(content, 'lxml')
-    products = single_page_soup.findAll("tr", {"class": "product-listitem"})
-    pre_str = "http://s.etao.com"
+    product_table = single_page_soup.findAll("table", {"class": "product-table"})[0]
+    product_trs = product_table.findAll("tr")
+    index = 0
     app_str = "?tab=comment#J_detail_tabs_label"
-    temp_urls = [pre_str + product.find("td", {"class": "cell2"}).a.get('href') + app_str for product in products]
-    print len(args)
-    args.extend(temp_urls)
+    for product_tr in product_trs:
+        if index > 0:
+            product_url = product_tr.findAll("td")[1].find("a").get("href") + app_str
+            all_phone_urls.append(product_url)
+        index += 1
+    workers.remove(worker_index)
+    print 'worker: ', worker_index, ' len(all_phone_urls) is: ', len(all_phone_urls)
+
+
+def save_page(file_name, content):
+    """ save the page, used to test whether the pages are the same
+    """
+    print '-------save %s --------' % file_name
+    save_file = codecs.open(file_name, 'w', 'gbk')
+    save_file.write(content)
+    save_file.close()
+
+
+def phone_total_num():
+    phone_file = open('all_phone_urls.txt', 'r')
+    all_urls = [line for line in phone_file.readlines()]
+    all_phone = []
+    for phone_url in all_urls:
+        all_phone.append(phone_url[phone_url.find('item/')+len('item/'): phone_url.find('.html?')])
+    print 'length of all_phone list is: ', len(all_phone)
+    all_phone_set = set(all_phone)
+    print 'length of all_phone set is: ', len(all_phone_set)
+    phone_file.close()
+    print '-----------------DONE-------------------------'
 
 
 def main():
@@ -72,4 +108,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    phone_total_num()
